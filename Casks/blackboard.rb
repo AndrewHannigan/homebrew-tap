75c79c9a8704ea0f66bf4ee691c688ff268c8f26
Andrew Hannigan <andrewhannigan@Andrews-MacBook-Air.local>
2026-01-03 18:06:36 -0600

    bump version

--- Casks/blackboard.rb
+++ Casks/blackboard.rb
@@ -1,5 +1,5 @@
 cask "blackboard" do
-  version "0.1.0"
+  version "0.2.0"
   sha256 "42939e4601da5e3515935dd8d3cf00b204eede9c7c84f49734b3387184c0293f"
 
   url "https://github.com/AndrewHannigan/blackboard/releases/download/v#{version}/Blackboard-#{version}-arm64.zip"
