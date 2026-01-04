57163b89e524a61da6b63a3c2bfc92238f19961b
Andrew Hannigan <andrew.s.hannigan@gmail.com>
2026-01-03 21:00:12 -0600

    bump

--- Casks/blackboard.rb
+++ Casks/blackboard.rb
@@ -1,9 +1,9 @@
 cask "blackboard" do
-  version "0.3.0"
-  sha256 "8f1dc49fbfae9ac69ada14eac657220d41693f7c7abda1da2925d5f14206dad3"
+  version "0.4.0"
+  sha256 "9cc42c1da265925037f1919ebf45e2808540c2f6a335f4493a30b7a42a872036"
 
   url "https://github.com/AndrewHannigan/blackboard/releases/download/v#{version}/Blackboard-#{version}-arm64.zip"
-  name "Whiteboard"
+  name "Blackboard"
   desc "Minimal scratch text editor"
   homepage "https://github.com/AndrewHannigan/blackboard"
 
