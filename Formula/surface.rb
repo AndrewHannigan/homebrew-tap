2df501e799af69f0adb21b5bfbff27413dcf5332
Andrew Hannigan <andrew.s.hannigan@gmail.com>
2026-03-22 18:25:23 -0500

    Use universal binary (arm64 + x86_64)

--- Formula/surface.rb
+++ Formula/surface.rb
@@ -2,7 +2,7 @@ class Surface < Formula
   desc "Float a draggable file thumbnail on your screen"
   homepage "https://github.com/AndrewHannigan/surface"
   url "https://github.com/AndrewHannigan/surface/releases/download/v0.1.1/surface"
-  sha256 "5b627a00e287667158c194dda50ca825f4ac968d1ec05d39df3d0cdff96104fd"
+  sha256 "e37b2d2038836d0c288f79350243c3be9225ee72942d6218542164497452a30c"
   version "0.1.1"
   license :cannot_represent
 
