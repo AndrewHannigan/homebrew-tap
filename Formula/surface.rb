f898989de383258a49efaa38149730aa0b313993
Andrew Hannigan <andrew.s.hannigan@gmail.com>
2026-03-22 18:46:50 -0500

    Update surface to v0.2.0

--- Formula/surface.rb
+++ Formula/surface.rb
@@ -1,9 +1,9 @@
 class Surface < Formula
   desc "Float a draggable file thumbnail on your screen"
   homepage "https://github.com/AndrewHannigan/surface"
-  url "https://github.com/AndrewHannigan/surface/releases/download/v0.1.1/surface.tar.gz"
-  sha256 "14264c019a630ebc9c7c0a2065f8aa53d21082343af767ecf15f2feb889f4f5b"
-  version "0.1.1"
+  url "https://github.com/AndrewHannigan/surface/releases/download/v0.2.0/surface.tar.gz"
+  sha256 "ba0bebe58f4025486112b3746a518d8c4d7bc514d07400271f63056fd5ce3515"
+  version "0.2.0"
   license :cannot_represent
 
   depends_on :macos
